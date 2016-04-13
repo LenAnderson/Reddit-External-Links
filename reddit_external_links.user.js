35af1ac58c7a4d4d96b4fc227f031e487d3be36f
Len <LenAnderson@users.noreply.github.com>
2016-04-13 13:54:05 +0200

    Update reddit_external_links.user.js

--- reddit_external_links.user.js
+++ reddit_external_links.user.js
@@ -2,14 +2,14 @@
 // @name         Reddit External Links
 // @namespace    https://github.com/LenAnderson/
 // @downloadURL  https://github.com/LenAnderson/Reddit-External-Links/raw/master/reddit_external_links.user.js
-// @version      1.0
+// @version      1.1
 // @match        https://www.reddit.com/*
 // @match        https://www.reddit.com
 // @grant        none
 // ==/UserScript==
 
-if (document.querySelector('p').textContent != '(error code: 503)') {
-    Array.prototype.forEach.call(document.querySelectorAll('.listing-page #siteTable > .thing.link > .entry'), function(entry) {
+function changeLinks() {
+    [].forEach.call(document.querySelectorAll('.listing-page #siteTable > .thing.link > .entry'), function(entry) {
         var link = entry.querySelector('a.title');
         if (link.href.search(/^https?:\/\/www.reddit.com/) == -1) {
             link.href = entry.querySelector('.buttons a.comments').href + '?followExternalLink';
@@ -17,9 +17,18 @@ if (document.querySelector('p').textContent != '(error code: 503)') {
             link.setAttribute('data-outbound-url', link.href);
         }
     });
+}
+
+if (document.querySelector('p').textContent != '(error code: 503)') {
+    changeLinks();
 
     if (location.search == '?followExternalLink') {
         history.replaceState({}, "", location.href.replace(/\?followExternalLink/, ''));
         document.querySelector('.single-page #siteTable > .thing.link > .entry a.title').click();
     }
+
+    var mo = new MutationObserver(function(muts) {
+        changeLinks();
+    });
+    mo.observe(document.body, {childList: true, subtree: true});
 }
